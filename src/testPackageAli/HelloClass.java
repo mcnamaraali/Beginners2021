9b5a53c753da23e2c4239e803f6961b73962294c
Alison McNamara <alison.mcnamara@pass-consulting.com>
2021-11-23 19:45:52 +0100

    Hello World

---
+++ src/testPackageAli/HelloClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package testPackageAli;
+
+/**
+ * @author Ali
+ *
+ */
+public class HelloClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+	}
+
+}
